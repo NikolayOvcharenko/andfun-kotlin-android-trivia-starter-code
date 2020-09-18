cc28705a9925cc4b004d9729909768d7ed873e62
Nikolay <56501319+NikolayOvcharenko@users.noreply.github.com>
2020-09-18 12:44:14 +0300

    add menu, rules and about

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -36,25 +36,25 @@ class GameFragment : Fragment() {
     // resources so we could internationalize. (or better yet, not define the questions in code...)
     private val questions: MutableList<Question> = mutableListOf(
             Question(text = "What is Android Jetpack?",
-                    answers = listOf("all of these", "tools", "documentation", "libraries")),
+                    answers = listOf("all of these*", "tools", "documentation", "libraries")),
             Question(text = "Base class for Layout?",
-                    answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")),
+                    answers = listOf("ViewGroup*", "ViewSet", "ViewCollection", "ViewRoot")),
             Question(text = "Layout for complex Screens?",
-                    answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")),
+                    answers = listOf("ConstraintLayout*", "GridLayout", "LinearLayout", "FrameLayout")),
             Question(text = "Pushing structured data into a Layout?",
-                    answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")),
+                    answers = listOf("Data Binding*", "Data Pushing", "Set Text", "OnClick")),
             Question(text = "Inflate layout in fragments?",
-                    answers = listOf("onCreateView", "onActivityCreated", "onCreateLayout", "onInflateLayout")),
+                    answers = listOf("onCreateView*", "onActivityCreated", "onCreateLayout", "onInflateLayout")),
             Question(text = "Build system for Android?",
-                    answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")),
+                    answers = listOf("Gradle*", "Graddle", "Grodle", "Groyle")),
             Question(text = "Android vector format?",
-                    answers = listOf("VectorDrawable", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
+                    answers = listOf("VectorDrawable*", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
             Question(text = "Android Navigation Component?",
-                    answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")),
+                    answers = listOf("NavController*", "NavCentral", "NavMaster", "NavSwitcher")),
             Question(text = "Registers app with launcher?",
-                    answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")),
+                    answers = listOf("intent-filter*", "app-registry", "launcher-registry", "app-launcher")),
             Question(text = "Mark a layout for Data Binding?",
-                    answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
+                    answers = listOf("<layout>*", "<binding>", "<data-binding>", "<dbinding>"))
     )
 
     lateinit var currentQuestion: Question
--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,11 +16,11 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -42,4 +42,38 @@ class GameWonFragment : Fragment() {
                 Toast.LENGTH_LONG).show()
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu, menu)
+        // проверка если активити разрешено
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    private fun getShareIntent(): Intent {
+        var args = GameWonFragmentArgs.fromBundle(arguments)
+        return ShareCompat.IntentBuilder.from(activity!!)
+                .setText(getString(R.string.share_success_text,args.numCorrect,args.numQuestions))
+                .setType("text/plain")
+                .intent
+
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.setType("text/plan")
+//                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text,args.numCorrect, args.numQuestions))
+//        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
